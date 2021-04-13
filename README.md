5aedfdf12242a15e59bf8c4d187aa5da60bc91aa
John Hamilton <john.hamilton.developer@gmail.com>
2021-04-13 12:33:14 +1000

    Database connection
    Got the database connection working and sending back to the api

--- README.md
+++ README.md
@@ -1,10 +1,28 @@
 # Development Environment
 
+tools local
+
+- nodejs
+- MySQL Community (Used for local testing of the database)
+
 ## backend
 
 In the commandline go to the backend folder 
 
 start server ```node app.js```
+
+### Setting up datebase
+
+The mysql database will have to be in legacy authentication mode.
+
+You will need to change this code to match the details for you database
+
+```
+host: '127.0.0.1',
+user: 'root',
+password: 'password',
+database: 'database',
+```
 
 ## frontend
 In the commandline go to the frontend folder 
--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,33 @@
 const express = require('express')
+require('dotenv').config()
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('welcome')
+});
+
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  password: 'password',
+  database: 'database',
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log('You are now connected with mysql database...')
 })
+
+app.get('/customer', function (req, res) {
+  connection.query('select * from persons', function (err, results, fields) {
+   if (err) throw err;
+   res.end(JSON.stringify(results));
+ });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
