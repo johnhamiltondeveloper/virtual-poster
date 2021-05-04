c2b8be714fb00451fc9f021a093eb25ab3b419ee
John Hamilton <john.hamilton.developer@gmail.com>
2021-05-04 22:08:37 +1000

    Update database_schema.sql

--- MySQL/database_schema.sql
+++ MySQL/database_schema.sql
@@ -1,12 +1,52 @@
+CREATE TABLE `animation` (
+  `frameID` int NOT NULL AUTO_INCREMENT,
+  `AnimationID` int DEFAULT NULL,
+  `locationX` int DEFAULT NULL,
+  `locationY` int DEFAULT NULL,
+  `scale` int DEFAULT NULL,
+  `time` int DEFAULT NULL,
+  PRIMARY KEY (`frameID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `attendees` (
+  `UserID` int NOT NULL,
+  `EventID` int NOT NULL,
+  PRIMARY KEY (`UserID`,`EventID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `event` (
+  `EventID` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `EventOwner` int DEFAULT NULL,
+  PRIMARY KEY (`EventID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `presentation` (
+  `PresentationID` int NOT NULL,
+  `video` varchar(100) DEFAULT NULL,
+  `pdf` varchar(100) DEFAULT NULL,
+  `AnimationID` int DEFAULT NULL,
+  `Name` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`PresentationID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `presentationowner` (
+  `OwnerID` int NOT NULL AUTO_INCREMENT,
+  `EventID` int DEFAULT NULL,
+  `UserID` int DEFAULT NULL,
+  `PresentationID` int DEFAULT NULL,
+  PRIMARY KEY (`OwnerID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 CREATE TABLE `users` (
-  `PersonID` int NOT NULL AUTO_INCREMENT,
+  `UserID` int NOT NULL AUTO_INCREMENT,
   `email` varchar(255) DEFAULT NULL,
   `password` varchar(100) DEFAULT NULL,
   `access_token` varchar(100) DEFAULT NULL,
   `name` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`PersonID`),
-  UNIQUE KEY `PersonID_UNIQUE` (`PersonID`),
+  `user_level` int DEFAULT '0',
+  PRIMARY KEY (`UserID`),
+  UNIQUE KEY `PersonID_UNIQUE` (`UserID`),
   UNIQUE KEY `email_UNIQUE` (`email`),
   UNIQUE KEY `access_token_UNIQUE` (`access_token`)
-)
-
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
